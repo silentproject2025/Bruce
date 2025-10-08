cda0b3b9bb433489c4580859411dc9dedc316fa6
silent project <hidayahihsan226@gmail.com>
2025-10-08 18:10:42 +0700

    Update display.cpp

--- src/core/display.cpp
+++ src/core/display.cpp
@@ -809,7 +809,7 @@ void drawStatusBar() {
 #endif
     } else {
         setTftDisplay(12, 12, bruceConfig.priColor, 1, bruceConfig.bgColor);
-       tft.print("Silent Project " + String(BRUCE_VERSION));
+       tft.print("Silent protocol " + String(BRUCE_VERSION));
     }
 }
 
